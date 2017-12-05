a43a5d401d1dba883fe635df745386754ee48552
Ryan Armstrong <rarmstrong@sofi.org>
2017-12-05 10:20:42 -0700

    broke out list projects and getting project information into two seperate methods and changed supporting structs.

--- projects.go
+++ projects.go
@@ -2,25 +2,19 @@ package bamboo
 
 import (
 	"fmt"
-	"io/ioutil"
 )
 
 // ProjectService handles communication with the project related methods
 type ProjectService service
 
-// ListProjectsOptions allows you to pass a project name to get information on
-type ListProjectsOptions struct {
-	Project string
-}
-
-// ProjectResponse wraps all the information about the projects
+// ProjectResponse the REST response from the server
 type ProjectResponse struct {
-	List Projects `json:"projects"`
+	Response Projects `json:"projects"`
 }
 
 // Projects is a collection of project elements
 type Projects struct {
-	List []Project `json:"project"`
+	ProjectList []Project `json:"project"`
 }
 
 // Project is a single project definition
@@ -30,15 +24,26 @@ type Project struct {
 	Description string `json:"description,omitempty"`
 }
 
-// ListProjects lists all projects unless ListProjectsOptions are passed
-func (p *ProjectService) ListProjects(opt *ListProjectsOptions) ([]Project, error) {
-	u := "project"
-	if opt != nil {
-		if !emptyStrings(opt.Project) {
-			u += fmt.Sprintf("/%s.json", opt.Project)
-		}
+// ProjectInformation is the information for a single project
+type ProjectInformation struct {
+	Key         string                  `json:"key,omitempty"`
+	Name        string                  `json:"name,omitempty"`
+	Description string                  `json:"description,omitempty"`
+	NumPlans    ProjectPlansInformation `json:"plans"`
+}
+
+// ProjectPlansInformation holds the number of plans in a project
+type ProjectPlansInformation struct {
+	Size int `json:"size,omitempty"`
+}
+
+// ProjectInfo get the information on the specific project
+func (p *ProjectService) ProjectInfo(projectKey string) (*ProjectInformation, error) {
+	var u string
+	if !emptyStrings(projectKey) {
+		u = fmt.Sprintf("project/%s.json", projectKey)
 	} else {
-		u += ".json"
+		return nil, &simpleError{fmt.Sprintf("Project key cannot be an empty string")}
 	}
 
 	req, err := p.client.NewRequest("GET", u, nil)
@@ -46,7 +51,24 @@ func (p *ProjectService) ListProjects(opt *ListProjectsOptions) ([]Project, erro
 		return nil, err
 	}
 
-	fmt.Println(req.URL.String())
+	projectInfo := ProjectInformation{}
+	resp, err := p.client.Do(req, &projectInfo)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return &projectInfo, nil
+}
+
+// ListProjects lists all projects
+func (p *ProjectService) ListProjects() ([]Project, error) {
+	u := "project.json"
+
+	req, err := p.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
 
 	projectResp := ProjectResponse{}
 	resp, err := p.client.Do(req, &projectResp)
@@ -55,14 +77,9 @@ func (p *ProjectService) ListProjects(opt *ListProjectsOptions) ([]Project, erro
 	}
 	defer resp.Body.Close()
 
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(bodyBytes))
-
 	if !(resp.StatusCode == 200) {
 		return nil, &simpleError{fmt.Sprintf("List projects returned %s", resp.Status)}
 	}
 
-	fmt.Println(len(projectResp.List.List))
-
-	return projectResp.List.List, nil
+	return projectResp.Response.ProjectList, nil
 }
