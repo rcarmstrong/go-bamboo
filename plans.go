735028fe7e36b32a8b18125ce2068fcac1338d18
Ryan Armstrong <rarmstrong@sofi.org>
2017-12-05 10:55:08 -0700

    added method to get number of plans and a list all plans info.

--- plans.go
+++ plans.go
@@ -14,6 +14,33 @@ type PlanCreateBranchOptions struct {
 	VCSBranch string
 }
 
+// PlanResponce encapsultes a response from the REST API
+type PlanResponce struct {
+	Response Plans `json:"plans"`
+}
+
+// Plans is a collection of Plan objects
+type Plans struct {
+	Size int    `json:"size"`
+	List []Plan `json:"plan"`
+}
+
+// Plan is the definition of a single plan
+type Plan struct {
+	ShortName string  `json:"shortName"`
+	ShortKey  string  `json:"shortKey"`
+	Type      string  `json:"type"`
+	Enabled   bool    `json:"enabled"`
+	Key       string  `json:"key"`
+	Name      string  `json:"name"`
+	PK        PlanKey `json:"planKey"`
+}
+
+// PlanKey holds the plan-key for a plan
+type PlanKey struct {
+	Key string `json:"key"`
+}
+
 // CreatePlanBranch will create a plan branch with the given branch name for the specified build
 func (p *PlanService) CreatePlanBranch(projectKey, buildKey, branchName string, opt *PlanCreateBranchOptions) (*http.Response, error) {
 	var u string
@@ -35,10 +62,64 @@ func (p *PlanService) CreatePlanBranch(projectKey, buildKey, branchName string, 
 	if err != nil {
 		return resp, err
 	}
+	defer resp.Body.Close()
 
 	if !(resp.StatusCode == 200) {
 		return resp, &simpleError{fmt.Sprintf("Create returned %d", resp.StatusCode)}
 	}
 
 	return resp, nil
+}
+
+// NumberOfPlans returns the number of plans on the Bamboo server
+func (p *PlanService) NumberOfPlans() (int, error) {
+	// Restrict results to one for speed
+	u := "plan.json?max-results=1"
+
+	req, err := p.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	planInfo := PlanResponce{}
+	resp, err := p.client.Do(req, &planInfo)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return 0, &simpleError{fmt.Sprintf("Getting the number of plans returned %s", resp.Status)}
+	}
+
+	return planInfo.Response.Size, nil
+}
+
+// ListPlans gets information on all plans
+func (p *PlanService) ListPlans() ([]Plan, error) {
+
+	numPlans, err := p.NumberOfPlans()
+	if err != nil {
+		return nil, err
+	}
+
+	u := fmt.Sprintf("plan.json?max-results=%d", numPlans)
+
+	req, err := p.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	planInfo := PlanResponce{}
+	resp, err := p.client.Do(req, &planInfo)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, &simpleError{fmt.Sprintf("Getting plan information returned %s", resp.Status)}
+	}
+
+	return planInfo.Response.List, nil
 }
