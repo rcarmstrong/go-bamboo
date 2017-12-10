0b07d13ada7a3fe9c6aae22d4a4f1f7417aa4a5f
Ryan Armstrong <cowboys6750@gmail.com>
2017-12-10 10:29:44 -0700

    added test files.

---
+++ client_test.go
@@ -0,0 +1,16 @@
+package bamboo_test
+
+// import (
+// 	"os"
+
+// 	bamboo "github.com/rcarmstrong/go-bamboo"
+// )
+
+// var (
+// 	bambooCLI *bamboo.Client
+// )
+
+// func init() {
+// 	b := bamboo.NewSimpleClient(nil, os.Getenv("BAMBOO_USERNAME"), os.Getenv("BAMBOO_PASSWORD"))
+// 	bambooCLI = b
+// }
--- plans_test.go
+++ plans_test.go
@@ -1,20 +1,8 @@
 package bamboo_test
 
 import (
-	"os"
 	"testing"
-
-	bamboo "github.com/rcarmstrong/go-bamboo"
 )
-
-var (
-	bambooCLI *bamboo.Client
-)
-
-func init() {
-	b := bamboo.NewSimpleClient(nil, os.Getenv("BAMBOO_USERNAME"), os.Getenv("BAMBOO_PASSWORD"))
-	bambooCLI = b
-}
 
 func TestNumberOfPlans(t *testing.T) {
 	numPlans, err := bambooCLI.Plans.NumberOfPlans()
@@ -25,7 +13,6 @@ func TestNumberOfPlans(t *testing.T) {
 }
 
 func TestListPlanKeys(t *testing.T) {
-	t.Log(bambooCLI.BaseURL.String())
 	keys, err := bambooCLI.Plans.ListPlanKeys()
 	if err != nil {
 		t.Errorf("Error listing plan keys %s", err)
