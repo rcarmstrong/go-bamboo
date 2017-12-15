589a29b0a1d9e42ec8c09afc67b1ad469012bef2
Ryan Armstrong <rarmstrong@sofi.org>
2017-12-15 10:36:05 -0700

    added error to handle bad URLs.

--- errors.go
+++ errors.go
@@ -7,3 +7,23 @@ type simpleError struct {
 func (e *simpleError) Error() string {
 	return e.message
 }
+
+func newSimpleError(message string) *simpleError {
+	return &simpleError{
+		message: message,
+	}
+}
+
+type errBadURL struct {
+	message string
+}
+
+func (e *errBadURL) Error() string {
+	return e.message
+}
+
+func newErrBadURL(message string) *errBadURL {
+	return &errBadURL{
+		message: message,
+	}
+}
