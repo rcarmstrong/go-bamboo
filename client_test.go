0e83fdc0607470b15646a36a86058e616202005d
Ryan Armstrong <rarmstrong@sofi.org>
2017-12-15 10:37:04 -0700

    test two cases where the SetURL should not error out and one that it should.

--- client_test.go
+++ client_test.go
@@ -19,7 +19,38 @@ func init() {
 }
 
 func TestSetURL(t *testing.T) {
+	testClient := bamboo.NewSimpleClient(nil, "myusername", "mypassword")
+	cases := []struct {
+		testURL     string
+		expected    string
+		shouldError bool
+	}{
+		{
+			testURL:     "http://localhost:8085",
+			expected:    "http://localhost:8085/rest/api/latest/",
+			shouldError: false,
+		},
+		{
+			testURL:     "fuzzybunnyslippers",
+			expected:    "fuzzybunnyslippers/rest/api/latest",
+			shouldError: true,
+		},
+	}
 
+	for _, c := range cases {
+		err := testClient.SetURL(c.testURL)
+		if err != nil {
+			if c.shouldError {
+				t.Logf("%s expected to throw an err %s", c.testURL, err.Error())
+			} else {
+				t.Error(err)
+			}
+		} else {
+			if testClient.BaseURL.String() != c.expected {
+				t.Errorf("Client BaseURL is %s when we expected %s", testClient.BaseURL.String(), c.expected)
+			}
+		}
+	}
 }
 
 func TestNewSimpleClient(t *testing.T) {
