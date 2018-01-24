f50a404a4891280c5fad9fa7e655afaba79478fd
Ryan Armstrong <cowboys6750@gmail.com>
2018-01-24 16:55:34 -0700

    added label service with add label method.

---
+++ labels.go
@@ -0,0 +1,45 @@
+package bamboo
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// LabelService handles communication with the labels on a plan result
+type LabelService service
+
+// Label is a single label on a result
+type Label struct {
+	Name      string `json:"name"`
+	ResultKey string `json:"-"`
+}
+
+func (lb Label) isEmpty() bool {
+	return lb.ResultKey == "" || lb.Name == ""
+}
+
+// AddLabel will add a label to the given result.
+func (c *LabelService) AddLabel(label *Label) (bool, *http.Response, error) {
+	if label == nil || label.isEmpty() {
+		return false, nil, &simpleError{"Label cannot be nil or empty"}
+	}
+	u := fmt.Sprintf("result/%s/label.json", label.ResultKey)
+
+	request, err := c.client.NewRequest("POST", u, label)
+	if err != nil {
+		return false, nil, err
+	}
+
+	request.Header.Add("Accept", "application/json")
+
+	response, err := c.client.Do(request, nil)
+	if err != nil {
+		return false, response, err
+	}
+
+	if !(response.StatusCode == 204) {
+		return false, response, &simpleError{fmt.Sprintf("Adding Label to %s returned %s", label.ResultKey, response.Status)}
+	}
+
+	return true, response, nil
+}
---
+++ labels_test.go
@@ -0,0 +1,57 @@
+package bamboo_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	bamboo "github.com/rcarmstrong/go-bamboo"
+)
+
+var (
+	testLabel      = "prod"
+	resultLabelKey = "TEST-TEST-1"
+)
+
+func TestAddLabel(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(addLabelStub))
+	defer ts.Close()
+
+	client := bamboo.NewSimpleClient(nil, "", "")
+	client.SetURL(ts.URL)
+
+	label := &bamboo.Label{
+		Name:      testLabel,
+		ResultKey: resultLabelKey,
+	}
+
+	success, resp, err := client.Labels.AddLabel(label)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if success == false || resp.StatusCode != 204 {
+		t.Error(fmt.Sprintf("Adding Label \"%s\" was unsuccessful. Returned %s", testLabel, resp.Status))
+	}
+}
+
+func addLabelStub(w http.ResponseWriter, r *http.Request) {
+	label := &bamboo.Label{}
+	expectedURI := fmt.Sprintf("/rest/api/latest/result/%s/label.json", resultLabelKey)
+
+	json.NewDecoder(r.Body).Decode(label)
+
+	if label.Name != testLabel {
+		http.Error(w, "Labels do not match", http.StatusBadRequest)
+		return
+	}
+
+	if r.RequestURI != expectedURI {
+		http.Error(w, fmt.Sprintf("URI did not match expected %s", resultLabelKey), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
