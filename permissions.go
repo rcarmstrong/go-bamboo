2834591cef6ffe9d6e250a77a3151f0d21a2f243
Ryan Armstrong <cowboys6750@gmail.com>
2018-03-22 07:53:34 -0600

    Created permission constants

---
+++ permissions.go
@@ -0,0 +1,16 @@
+package bamboo
+
+// WritePermission is the sting the API expects for write permissions
+const WritePermission = "WRITE"
+
+// BuildPermission is the sting the API expects for the ability to build a plan
+const BuildPermission = "BUILD"
+
+// ReadPermission is the sting the API expects for read permissions
+const ReadPermission = "READ"
+
+// ClonePermission is the sting the API expects to be able to clone a plan
+const ClonePermission = "CLONE"
+
+// AdminPermission is the sting the API expects for admin permissions
+const AdminPermission = "ADMINISTRATION"
