23dbb8349ca8c67f9db3576095a6c6b58b930e44
Ryan Armstrong <cowboys6750@gmail.com>
2018-05-16 10:52:27 -0600

    moved ProjectPlan struct into permissions.go

--- permissions.go
+++ permissions.go
@@ -24,3 +24,7 @@ const AdminPermission string = "ADMINISTRATION"
 type Permissions struct {
 	ProjectPlan *ProjectPlanService
 }
+
+// ProjectPlanService handles communication with API endpoints that dictate the permissions for
+// users/groups/roles at the project level which are inherited by the projects plans.
+type ProjectPlanService service
