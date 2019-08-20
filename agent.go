8ecb30794499be28491cf206530da57db9fecfc2
Ryan Armstrong <rarmstrong@overstock.com>
2019-08-20 16:19:51 -0600

    finished DedicateAgent and RemoveAgentAssignment methods.

--- agent.go
+++ agent.go
@@ -29,7 +29,12 @@ var knownExecutorTypes = map[string]bool{
 	ExecutorTypeImage: true,
 }
 
-var knownAssignmentTypes = map[string]bool{}
+var knownAssignmentTypes = map[string]bool{
+	AssignmentTypeEnvironment: true,
+	AssignmentTypeJob:         true,
+	AssignmentTypePlan:        true,
+	AssignmentTypeProject:     true,
+}
 
 // AgentService handles communication with the agent related endpoints
 type AgentService service
@@ -47,7 +52,7 @@ type AgentAssignmentInformation struct {
 	CapabilitiesMatch   bool     `json:"capabilitiesMatch,omitempty"`
 }
 
-// GetAgentAssignments gets the current assignment information for the given executor (aka agent)
+// GetAgentAssignments gets the current assignment information for the given executor (agent)
 func (a *AgentService) GetAgentAssignments(executorType string, executorID int64) ([]*AgentAssignmentInformation, *http.Response, error) {
 	var u string
 	if !emptyStrings(executorType) {
@@ -79,8 +84,8 @@ func (a *AgentService) GetAgentAssignments(executorType string, executorID int64
 	return agentAssignmentInformation, response, nil
 }
 
-// DedicateAgent assigns the given executor to the given entity
-func (a *AgentService) DedicateAgent(executorType, assignmentType string, executorID, entityID int64) ([]*AgentAssignmentInformation, *http.Response, error) {
+// DedicateAgent assigns the given executor (agent) to the given entity
+func (a *AgentService) DedicateAgent(executorType, assignmentType string, executorID, entityID int64) (*AgentAssignmentInformation, *http.Response, error) {
 	var u string
 	if !emptyStrings(executorType, assignmentType) {
 		if knownExecutorTypes[executorType] {
@@ -96,7 +101,7 @@ func (a *AgentService) DedicateAgent(executorType, assignmentType string, execut
 		return nil, nil, &simpleError{fmt.Sprintf("ExecutorType or AssignmentType cannot be an empty string")}
 	}
 
-	request, err := a.client.NewRequest(http.MethodGet, u, nil)
+	request, err := a.client.NewRequest(http.MethodPost, u, nil)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -105,14 +110,60 @@ func (a *AgentService) DedicateAgent(executorType, assignmentType string, execut
 	values.Set("executorType", executorType)
 	values.Set("executorId", strconv.FormatInt(executorID, 10))
 	values.Set("assignmentType", assignmentType)
-	values.Set("entityID", strconv.FormatInt(entityID, 10))
+	values.Set("entityId", strconv.FormatInt(entityID, 10))
 	request.URL.RawQuery = values.Encode()
 
-	agentAssignmentInformation := []*AgentAssignmentInformation{}
+	agentAssignmentInformation := AgentAssignmentInformation{}
 	response, err := a.client.Do(request, &agentAssignmentInformation)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	return agentAssignmentInformation, response, nil
+	if response.StatusCode != http.StatusOK {
+		return nil, response, &simpleError{fmt.Sprintf("DedicateAgent call returned: %d", response.StatusCode)}
+	}
+
+	return &agentAssignmentInformation, response, nil
+}
+
+// RemoveAgentAssignment removes the specified executor (agent) assignmnt from the given entity
+func (a *AgentService) RemoveAgentAssignment(executorType, assignmentType string, executorID, entityID int64) (*AgentAssignmentInformation, *http.Response, error) {
+	var u string
+	if !emptyStrings(executorType, assignmentType) {
+		if knownExecutorTypes[executorType] {
+			if knownAssignmentTypes[assignmentType] {
+				u = "agent/assignment"
+			} else {
+				return nil, nil, &simpleError{fmt.Sprintf("Unknown AssignmentType: %s", assignmentType)}
+			}
+		} else {
+			return nil, nil, &simpleError{fmt.Sprintf("Unknown ExecutorType: %s", executorType)}
+		}
+	} else {
+		return nil, nil, &simpleError{fmt.Sprintf("ExecutorType or AssignmentType cannot be an empty string")}
+	}
+
+	request, err := a.client.NewRequest(http.MethodDelete, u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	values := request.URL.Query()
+	values.Set("executorType", executorType)
+	values.Set("executorId", strconv.FormatInt(executorID, 10))
+	values.Set("assignmentType", assignmentType)
+	values.Set("entityId", strconv.FormatInt(entityID, 10))
+	request.URL.RawQuery = values.Encode()
+
+	agentAssignmentInformation := AgentAssignmentInformation{}
+	response, err := a.client.Do(request, &agentAssignmentInformation)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if response.StatusCode != http.StatusNoContent {
+		return nil, response, &simpleError{fmt.Sprintf("DedicateAgent call returned: %d", response.StatusCode)}
+	}
+
+	return &agentAssignmentInformation, response, nil
 }
